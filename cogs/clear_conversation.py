238b36378a2e64b3038aa8a8b3226350c1571a4b
Saverio <personnex976@gamil.com>
2021-08-13 14:50:26 +0200

    [feature] you can now delete all message of a conversation with the bot

--- cogs/clear_conversation.py
+++ cogs/clear_conversation.py
@@ -37,5 +37,26 @@ class ClearTask(commands.Cog):
         del self.bot.conversations[guild][user]
         await ctx.send('Conversation history delete!')
 
+    @commands.guild_only()
+    @commands.command()
+    @commands.bot_has_permissions(manage_messages=True, read_message_history=True)
+    async def clear_message_channel(self, ctx:commands.Context):
+        """Clear your message and bot's message if it respond to you."""
+        async with ctx.typing():
+            counter = 0
+            async for message in ctx.channel.history(oldest_first=False):
+                if message.author.id == ctx.author.id:
+                    if message.content.startswith('.pt '):
+                        await message.delete()
+                        counter += 1
+                if message.author.id == self.bot.user.id:
+                    refer = message.reference
+                    if refer != None:
+                        if refer.resolved != None:
+                            if refer.resolved.author.id == ctx.author.id:
+                                await message.delete()
+                                counter += 1
+            await ctx.send(f'delete {counter} message(s)')
+
 def setup(bot:commands.Bot):
     bot.add_cog(ClearTask(bot))
