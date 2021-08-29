aa45c671939caaf176897163b91462e0bb8d556a
Saverio <personnex976@gamil.com>
2021-08-29 22:52:56 +0200

    add a status

--- main.py
+++ main.py
@@ -13,13 +13,19 @@ from pyclass import send_webhook
 # Discord Bot
 import hikari
 import lightbulb
+from hikari import presences
 
 load_dotenv()
 
 TOKEN = os.environ["DISCORD_TOKEN"]
 INTENTS = hikari.Intents.ALL
 
-bot = lightbulb.Bot(prefix='.', token=TOKEN, intents=INTENTS, logs="ERROR")
+bot = lightbulb.Bot(
+    prefix='.',
+    token=TOKEN,
+    intents=INTENTS,
+    logs="ERROR"
+)
 bot._chatbot_send = send_webhook.SendWebhook()
 # Transformers
 # pre_trained possibility : microsoft/DialoGPT-large | microsoft/DialoGPT-medium | microsoft/DialoGPT-small
@@ -44,4 +50,9 @@ for file in os.listdir('./slash_plugins'):
         bot.load_extension(f'slash_plugins.{file[:-3]}')
         print(f'added :: {file}')
 
-bot.run()
+bot.run(
+    activity=presences.Activity(
+        name='with human | /help',
+        type=presences.ActivityType.PLAYING
+    )
+)
