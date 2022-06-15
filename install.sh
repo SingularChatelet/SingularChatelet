563d9d9e18d6758d793765f4242f503caec59346
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-15 11:31:23 +0000

    Adapt start and install script to accept not install all dependencies

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "./install.sh [--not-full]"
+    exit 0
+fi
 python3 -m pip install -r requirements.txt
+if [[ "$1" != "--not-full" ]]; then
+    python3 -m pip install -r requirements_full.txt
+fi
 python3 -m spacy download en_core_web_sm
--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-./install.sh
-
+#!/bin/bash
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "./start.sh"
+    echo "first argument is forwarded to ./install.sh"
+    exit 0
+fi
+./install.sh "$1"
 python3 ./main.py
