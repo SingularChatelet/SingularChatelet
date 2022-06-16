4a07418f1a4f033d11bdc5c2dd2e78f2cb7207bf
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-16 18:10:58 +0000

    Add use of cheat.sh

--- plugins/instant_answers.py
+++ plugins/instant_answers.py
@@ -1,4 +1,5 @@
 import requests
+import cheat_sh
 from urllib import parse
 
 import asyncio
@@ -66,6 +67,33 @@ async def smth(context: lightbulb.Context) -> None:
         text='duckducgo instant answer',
         icon='https://duckduckgo.com/favicon.ico'
     )
+    await res.edit(embed=emb)
+
+@plugin.command
+@lightbulb.option("question", "Question related to programing languages", required=True)
+@lightbulb.command("chtsh", "Get response from http://cheat.sh")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def chtsh(context: lightbulb.Context) -> None:
+    loop = asyncio.get_event_loop()
+    text = await loop.run_in_executor(
+        ThreadPoolExecutor(),
+        cheat_sh.ask,
+        context.options['question']
+    )
+    strips = context.options['question'].split()
+    if len(strips) != 0 and strips[0] in cheat_sh.get_supported_language():
+        language = strips[0]
+    else:
+        language = ""
+    language = language.lower()
+    print(language)
+    emb = hikari.Embed(
+        title=f'Cheat.sh Answer {context.options["question"]}',
+        description=f'```{language}\n{text}```',
+        color=0x2ae53f
+    )
+    res = await context.respond(emb)
+    emb.set_footer(text='cheat.sh answer')
     await res.edit(embed=emb)
 
 def load(bot: Bot):
