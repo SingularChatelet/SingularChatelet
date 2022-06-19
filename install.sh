0fc5eca0dfceffef2600ff2b6a83533e1836a137
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-19 14:39:50 +0000

    Install use 3 files

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "./install.sh [--not-full]"
+    echo "./install.sh [--no-transformers] [--no-chatterbot]"
     exit 0
 fi
 python3 -m pip install -r requirements.txt
-if [[ "$1" != "--not-full" ]]; then
-    python3 -m pip install -r requirements_full.txt
+if [[ $(echo "$@" | grep -e "--no-transformers") == "" ]]; then
+    python3 -m pip install -r ./requirements_transformers.txt
 fi
-python3 -m spacy download en_core_web_sm
+if [[ $(echo "$@" | grep -e "--no-chatterbot") == "" ]]; then
+    python3 -m pip install -r ./requirements_chatterbot.txt
+    python3 -m spacy download en_core_web_sm
+fi
